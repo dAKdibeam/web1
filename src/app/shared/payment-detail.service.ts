499f09f0f11e24e7d14e27d137c3b5e10985ff3b
dAKdibeam <kriskorn2u@gmail.com>
2019-08-26 23:35:26 +0700

    first commit

---
+++ src/app/shared/payment-detail.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { PaymentDetail } from './payment-detail.model';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentDetailService {
+  formData: PaymentDetail;
+  readonly rootURL = 'http://localhost:61103/api';
+
+  constructor(private http:HttpClient) { }
+
+  postPaymentDetail(formData:PaymentDetail){
+   return this.http.post(this.rootURL+'/PaymentDetail',formData);
+  }
+}
